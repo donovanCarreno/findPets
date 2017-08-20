8c5e2f355d10acb8301261aec8045019b6e26c56
donovanCarreno <donovan_carreno@yahoo.com>
2017-08-19 19:32:29 -0500

    remove api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import jsonp from 'jsonp'
 import data from '../../data'
+import KEY from '../../key'
 
 import {
   GET_PETS
 } from './types'
 
 const URL = 'http://api.petfinder.com'
-const KEY = '5e5567cb4a7a2ce053f34fe56b5c1081'
+const KEY = KEY
 
 export const getPets = () => {
   return (dispatch) => {
